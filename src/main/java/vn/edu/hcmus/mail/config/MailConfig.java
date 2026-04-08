a65f625bccf8f8408cc2ac06b9648d9ac0fe632c
Hadesghostkiller <hadesghostkiller@gmail.com>
2026-04-08 09:13:21 +0700

    đổi mail và app pass

--- src/main/java/vn/edu/hcmus/mail/config/MailConfig.java
+++ src/main/java/vn/edu/hcmus/mail/config/MailConfig.java
@@ -3,8 +3,8 @@ package vn.edu.hcmus.mail.config;
 public class MailConfig {
     public static final String SMTP_HOST = "smtp.gmail.com";
     public static final int SMTP_PORT = 587; // Cổng TLS
-    public static final String EMAIL_USER = "nguoigiachet200@gmail.com";
-    public static final String APP_PASSWORD = "yitmyagjpjslrddv";
+    public static final String EMAIL_USER = "doanltmang@gmail.com";
+    public static final String APP_PASSWORD = "sowyawumjpziavyb";
     public static final String POP3_HOST = "pop.gmail.com";
     public static final int POP3_PORT = 995;
     public static final String PROTOCOL = "pop3s"; // pop3 có bảo mật (SSL)
